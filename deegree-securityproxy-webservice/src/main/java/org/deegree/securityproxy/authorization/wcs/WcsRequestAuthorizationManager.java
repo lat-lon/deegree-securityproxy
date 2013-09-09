1aca70454b7845e2931ffa723ccd38ab256fd2ef
lgoltz <goltz@lat-lon.de>
2013-09-09 15:23:20 +0200

    implemented WcsRequestAuthorizationManager

--- deegree-securityproxy-webservice/src/main/java/org/deegree/securityproxy/authorization/wcs/WcsRequestAuthorizationManager.java
+++ deegree-securityproxy-webservice/src/main/java/org/deegree/securityproxy/authorization/wcs/WcsRequestAuthorizationManager.java
@@ -4,11 +4,15 @@ import java.util.Collection;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.deegree.securityproxy.authentication.wcs.WcsPermission;
+import org.deegree.securityproxy.commons.WcsOperationType;
+import org.deegree.securityproxy.request.WcsRequest;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 
 /**
  * Checks if a authenticated User is permitted to perform an incoming {@link HttpServletRequest} against a WCS.
@@ -25,9 +29,53 @@ public class WcsRequestAuthorizationManager implements AccessDecisionManager {
     @Override
     public void decide( Authentication authentication, Object object, Collection<ConfigAttribute> configAttributes )
                             throws AccessDeniedException, InsufficientAuthenticationException {
-        // HttpServletRequest req = (HttpServletRequest) object;
+        WcsRequest wcsRequest = (WcsRequest) object;
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        for ( GrantedAuthority authority : authorities ) {
+            if ( isAuthorized( authority, wcsRequest ) ) {
+                return;
+            }
+        }
         // Shall support Authentication instances that contain WcsPermission(s) as GrantedAuthority(ies)
-        throw new UnsupportedOperationException();
+        throw new AccessDeniedException( "Unauthorizised" );
+    }
+
+    private boolean isAuthorized( GrantedAuthority authority, WcsRequest wcsRequest ) {
+        if ( authority instanceof WcsPermission ) {
+            WcsPermission wcsPermission = (WcsPermission) authority;
+            
+            if ( isGetCoverageRequest( wcsRequest ) ) {
+                if ( !isLayerNameAuthorized( wcsRequest, wcsPermission ) )
+                    return false;
+            }
+            if ( isOperationTypeAuthorized( wcsRequest, wcsPermission )
+                 && isServiceVersionAuthorized( wcsRequest, wcsPermission )
+                 && isServiceNameAuthorized( wcsRequest, wcsPermission ) ) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    private boolean isGetCoverageRequest( WcsRequest wcsRequest ) {
+        return WcsOperationType.GETCOVERAGE.equals( wcsRequest.getOperationType() );
+    }
+
+    private boolean isOperationTypeAuthorized( WcsRequest wcsRequest, WcsPermission wcsPermission ) {
+        return wcsRequest.getOperationType().equals( wcsPermission.getOperationType() );
+    }
+
+    private boolean isServiceVersionAuthorized( WcsRequest wcsRequest, WcsPermission wcsPermission ) {
+        return wcsRequest.getServiceVersion().equals( wcsPermission.getServiceVersion() );
+    }
+
+    private boolean isServiceNameAuthorized( WcsRequest wcsRequest, WcsPermission wcsPermission ) {
+        return wcsRequest.getServiceName().equals( wcsPermission.getServiceName() );
+    }
+
+    private boolean isLayerNameAuthorized( WcsRequest wcsRequest, WcsPermission wcsPermission ) {
+        return wcsRequest.getLayerName().equals( wcsPermission.getLayerName() );
     }
 
     @Override
@@ -38,8 +86,7 @@ public class WcsRequestAuthorizationManager implements AccessDecisionManager {
 
     @Override
     public boolean supports( Class<?> clazz ) {
-        // Shall support HttpServletRequests for service type WCS
-        return false;
+        return WcsRequest.class.equals( clazz );
     }
 
 }
