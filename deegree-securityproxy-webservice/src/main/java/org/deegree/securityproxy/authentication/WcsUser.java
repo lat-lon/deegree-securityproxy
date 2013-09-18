ffe78b9b291d3c03882216c8bed70b07948c260f
Lyn Goltz <goltz@lat-lon.de>
2013-09-18 09:21:04 +0200

    added javadoc and tests

--- deegree-securityproxy-webservice/src/main/java/org/deegree/securityproxy/authentication/WcsUser.java
+++ deegree-securityproxy-webservice/src/main/java/org/deegree/securityproxy/authentication/WcsUser.java
@@ -1,17 +1,28 @@
 package org.deegree.securityproxy.authentication;
 
-import java.util.Collections;
+import static java.util.Collections.unmodifiableList;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import org.deegree.securityproxy.authentication.wcs.WcsPermission;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+/**
+ * {@link UserDetails} implementation encapsulating username, password, authorities ({@link WcsPermission}s) and
+ * {@link WcsGeometryFilterInfo}s.
+ * 
+ * @author <a href="erben@lat-lon.de">Alexander Erben</a>
+ * @author <a href="stenger@lat-lon.de">Dirk Stenger</a>
+ * @author <a href="goltz@lat-lon.de">Lyn Goltz</a>
+ * @author last edited by: $Author: lyn $
+ * 
+ * @version $Revision: $, $Date: $
+ */
 public class WcsUser implements UserDetails {
 
     private static final long serialVersionUID = 1264359266739783359L;
-
-    private final List<WcsGeometryFilterInfo> filters;
 
     private final String username;
 
@@ -19,12 +30,28 @@ public class WcsUser implements UserDetails {
 
     private final List<WcsPermission> authorities;
 
+    private final List<WcsGeometryFilterInfo> filters;
+
+    /**
+     * @param username
+     *            may be <code>null</code>
+     * @param password
+     *            may be <code>null</code>
+     * @param authorities
+     *            may be <code>null</code> or empty
+     * @param filters
+     *            may be <code>null</code> or empty
+     */
     public WcsUser( String username, String password, List<WcsPermission> authorities,
                     List<WcsGeometryFilterInfo> filters ) {
         this.username = username;
         this.password = password;
-        this.authorities = authorities;
-        this.filters = filters;
+        if ( authorities == null )
+            authorities = new ArrayList<WcsPermission>();
+        this.authorities = unmodifiableList( authorities );
+        if ( filters == null )
+            filters = new ArrayList<WcsGeometryFilterInfo>();
+        this.filters = unmodifiableList( filters );
     }
 
     @Override
@@ -62,12 +89,18 @@ public class WcsUser implements UserDetails {
         return true;
     }
 
+    /**
+     * @return authorities as list of {@link WcsPermission}s in a unmodifiable list, never <code>null</code>
+     */
     public List<WcsPermission> getWcsPermissions() {
         return authorities;
     }
 
-    public List<WcsGeometryFilterInfo> getResponseFilters() {
-        return Collections.unmodifiableList( filters );
+    /**
+     * @return all {@link WcsGeometryFilterInfo}s in a unmodifiable list, never <code>null</code>
+     */
+    public List<WcsGeometryFilterInfo> getWcsGeomtryFilterInfos() {
+        return filters;
     }
 
 }
