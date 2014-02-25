e7790503e1c7b6023ec85934893f1b90a27144ac
Dirk Stenger <stenger@lat-lon.de>
2014-02-25 10:00:16 +0100

    Added javadoc to ServiceManager

--- deegree-securityproxy-core/src/main/java/org/deegree/securityproxy/filter/ServiceManager.java
+++ deegree-securityproxy-core/src/main/java/org/deegree/securityproxy/filter/ServiceManager.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
+ * A {@link ServiceManager} encapsulates one service type. A {@link ServiceManager} contains all information to filter a
+ * specific service type.
+ *
  * @author <a href="goltz@lat-lon.de">Lyn Goltz</a>
  * @author <a href="stenger@lat-lon.de">Dirk Stenger</a>
  * @author last edited by: $Author: stenger $
@@ -17,14 +20,51 @@ import java.io.IOException;
  */
 public interface ServiceManager {
 
+    /**
+     * Parse a given request.
+     *
+     * @param httpRequest never <code>null</code>.
+     * @return
+     * @throws UnsupportedRequestTypeException when the given request does not have the correct service type.
+     */
     OwsRequest parse( HttpServletRequest httpRequest ) throws UnsupportedRequestTypeException;
 
+    /**
+     * Authorize a given request.
+     *
+     * @param authentication the user authentication to authorize, may be <code>null</code>.
+     * @param owsRequest     the secured request, never <code>null</code>.
+     * @return
+     */
     AuthorizationReport authorize( Authentication authentication, OwsRequest owsRequest );
 
+    /**
+     * Check if a response filter is enabled for a given request.
+     *
+     * @param owsRequest never <code>null</code>.
+     * @return
+     */
     boolean isResponseFilterEnabled( OwsRequest owsRequest );
 
+    /**
+     * Filter a response.
+     *
+     * @param wrappedResponse the response to filter, the response may be adjusted during filtering, never
+     *                        <code>null</code>.
+     * @param authentication  may be <code>null</code>.
+     * @param owsRequest      parsed request, never <code>null</code>.
+     * @return
+     * @throws IOException if an I/O error occurred during writing in the real output stream.
+     */
     ResponseFilterReport filterResponse( StatusCodeResponseBodyWrapper wrappedResponse,
                                          Authentication authentication, OwsRequest owsRequest ) throws IOException;
 
+    /**
+     * Check if a given request is supported by the {@link ServiceManager}.
+     *
+     * @param request never <code>null</code>.
+     * @return
+     */
     boolean isServiceTypeSupported( HttpServletRequest request );
+
 }
