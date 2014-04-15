c8d646ecd0ccf3e41da481beb72b5eea4bce3434
Dirk Stenger <stenger@lat-lon.de>
2014-04-15 10:25:47 +0200

    Added assertions comparing pixels to WmsClipperTest

--- deegree-securityproxy-service-wms/src/test/java/org/deegree/securityproxy/wms/responsefilter/clipping/WmsClipperTest.java
+++ deegree-securityproxy-service-wms/src/test/java/org/deegree/securityproxy/wms/responsefilter/clipping/WmsClipperTest.java
@@ -1,13 +1,24 @@
 package org.deegree.securityproxy.wms.responsefilter.clipping;
 
+import static javax.imageio.ImageIO.read;
+import static org.junit.Assert.assertThat;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.PixelGrabber;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Arrays;
+
+import javax.imageio.ImageIO;
 
 import org.deegree.securityproxy.service.commons.responsefilter.clipping.ImageClipper;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
 import com.vividsolutions.jts.geom.Envelope;
@@ -39,6 +50,8 @@ public class WmsClipperTest {
 
         inputStream.close();
         outputStream.close();
+
+        assertThat( destinationFile, hasNotSamePixels( sourceFile ) );
     }
 
     @Test
@@ -54,6 +67,8 @@ public class WmsClipperTest {
 
         inputStream.close();
         outputStream.close();
+
+        assertThat( destinationFile, hasNotSamePixels( sourceFile ) );
     }
 
     private File createNewFile( String resourceName ) {
@@ -83,6 +98,52 @@ public class WmsClipperTest {
     private Geometry createVisibleArea() {
         Envelope envelope = new Envelope( 10, 20, 10, 20 );
         return new GeometryFactory().toGeometry( envelope );
+    }
+
+    private Matcher<File> hasNotSamePixels( File sourceFile )
+                            throws Exception {
+
+        BufferedImage sourceImage = ImageIO.read( sourceFile );
+        PixelGrabber sourceGrabber = new PixelGrabber( sourceImage, 0, 0, -1, -1, false );
+
+        int[] sourcePixels = null;
+        if ( sourceGrabber.grabPixels() ) {
+            int width = sourceGrabber.getWidth();
+            int height = sourceGrabber.getHeight();
+            sourcePixels = new int[width * height];
+            sourcePixels = (int[]) sourceGrabber.getPixels();
+        }
+        final int[] sourcePixelsToCompare = sourcePixels;
+
+        return new BaseMatcher<File>() {
+
+            @Override
+            public boolean matches( Object item ) {
+                BufferedImage destinationImage;
+                try {
+                    destinationImage = read( (File) item );
+                    PixelGrabber destinationGrabber = new PixelGrabber( destinationImage, 0, 0, -1, -1, false );
+
+                    int[] destinationPixels = null;
+                    if ( destinationGrabber.grabPixels() ) {
+                        int width = destinationGrabber.getWidth();
+                        int height = destinationGrabber.getHeight();
+                        destinationPixels = new int[width * height];
+                        destinationPixels = (int[]) destinationGrabber.getPixels();
+                    }
+
+                    return !Arrays.equals( destinationPixels, sourcePixelsToCompare );
+                } catch ( Exception e ) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+
+            @Override
+            public void describeTo( Description description ) {
+                description.appendText( "Should not contain the same pixels as the source!" );
+            }
+        };
     }
 
 }
