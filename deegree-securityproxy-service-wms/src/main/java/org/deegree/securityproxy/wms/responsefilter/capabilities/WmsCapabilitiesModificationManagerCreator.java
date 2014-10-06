f7b3ad93154d9f4859a65b3260cc39cf8272c656
Dirk Stenger <stenger@lat-lon.de>
2014-10-06 16:22:38 +0200

    Extended WmsCapabilitiesModificationManagerCreator to replace dcp urls

--- deegree-securityproxy-service-wms/src/main/java/org/deegree/securityproxy/wms/responsefilter/capabilities/WmsCapabilitiesModificationManagerCreator.java
+++ deegree-securityproxy-service-wms/src/main/java/org/deegree/securityproxy/wms/responsefilter/capabilities/WmsCapabilitiesModificationManagerCreator.java
@@ -35,24 +35,30 @@
  ----------------------------------------------------------------------------*/
 package org.deegree.securityproxy.wms.responsefilter.capabilities;
 
-import static org.deegree.securityproxy.wms.request.WmsRequestParser.GETFEATUREINFO;
-import static org.deegree.securityproxy.wms.request.WmsRequestParser.GETMAP;
-import static org.deegree.securityproxy.wms.request.WmsRequestParser.VERSION_130;
-import static org.deegree.securityproxy.wms.request.WmsRequestParser.WMS_SERVICE;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
 import org.deegree.securityproxy.authentication.ows.raster.RasterPermission;
 import org.deegree.securityproxy.request.OwsRequest;
 import org.deegree.securityproxy.request.OwsServiceVersion;
 import org.deegree.securityproxy.service.commons.responsefilter.capabilities.DecisionMaker;
-import org.deegree.securityproxy.service.commons.responsefilter.capabilities.XmlModificationManagerCreator;
 import org.deegree.securityproxy.service.commons.responsefilter.capabilities.XmlModificationManager;
+import org.deegree.securityproxy.service.commons.responsefilter.capabilities.XmlModificationManagerCreator;
 import org.deegree.securityproxy.service.commons.responsefilter.capabilities.blacklist.BlackListDecisionMaker;
+import org.deegree.securityproxy.service.commons.responsefilter.capabilities.element.ElementPathStep;
+import org.deegree.securityproxy.service.commons.responsefilter.capabilities.text.AttributeModificationRule;
+import org.deegree.securityproxy.service.commons.responsefilter.capabilities.text.AttributeModifier;
+import org.deegree.securityproxy.service.commons.responsefilter.capabilities.text.StaticAttributeModifier;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+
+import javax.xml.namespace.QName;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.deegree.securityproxy.wms.request.WmsRequestParser.GETFEATUREINFO;
+import static org.deegree.securityproxy.wms.request.WmsRequestParser.GETMAP;
+import static org.deegree.securityproxy.wms.request.WmsRequestParser.VERSION_130;
+import static org.deegree.securityproxy.wms.request.WmsRequestParser.WMS_SERVICE;
 
 /**
  * Creates {@link DecisionMaker}s for WMS capabilities requests.
@@ -64,6 +70,10 @@ import org.springframework.security.core.GrantedAuthority;
  */
 public class WmsCapabilitiesModificationManagerCreator implements XmlModificationManagerCreator {
 
+    private String getDcpUrl;
+
+    private String postDcpUrl;
+
     private static final OwsServiceVersion VERSION_1_3_0 = new OwsServiceVersion( 1, 3, 0 );
 
     static final String WMS_1_3_0_NS_URI = "http://www.opengis.net/wms";
@@ -72,12 +82,33 @@ public class WmsCapabilitiesModificationManagerCreator implements XmlModificatio
 
     private static final String SUB_ELEMENT_NAME = "Name";
 
-    @Override
-    public XmlModificationManager createXmlModificationManager( OwsRequest owsRequest, Authentication authentication ) {
-        return new XmlModificationManager( createDecisionMaker( owsRequest, authentication ) );
+    /**
+     * Instantiates a new {@link WmsCapabilitiesModificationManagerCreator}. DCP url is not replaced!
+     */
+    public WmsCapabilitiesModificationManagerCreator() {
+        this( null, null );
     }
 
-    public DecisionMaker createDecisionMaker( OwsRequest owsRequest, Authentication authentication ) {
+    /**
+     *
+     * @param getDcpUrl
+     *            the new HTTP GET DCP URL replacing all DCP urls in the capabilities document, if <code>null</code>
+     *            nothing is replaced
+     * @param postDcpUrl
+     *            the new HTTP POST DCP URL replacing all DCP urls in the capabilities document, if <code>null</code>
+     *            nothing is replaced
+     */
+    public WmsCapabilitiesModificationManagerCreator( String getDcpUrl, String postDcpUrl ) {
+        this.getDcpUrl = getDcpUrl;
+        this.postDcpUrl = postDcpUrl;
+    }
+
+    @Override
+    public XmlModificationManager createXmlModificationManager( OwsRequest owsRequest, Authentication authentication ) {
+        return new XmlModificationManager( createDecisionMaker( owsRequest, authentication ), createAttributeModifier() );
+    }
+
+    private DecisionMaker createDecisionMaker( OwsRequest owsRequest, Authentication authentication ) {
         checkVersion( owsRequest );
         List<String> blackListLayerNames = new ArrayList<String>();
         Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
@@ -85,7 +116,19 @@ public class WmsCapabilitiesModificationManagerCreator implements XmlModificatio
             addBlackListValuesFromAuthorities( blackListLayerNames, grantedAuthority );
         }
         return new BlackListDecisionMaker( ELEMENT_TO_FILTER, WMS_1_3_0_NS_URI, SUB_ELEMENT_NAME, WMS_1_3_0_NS_URI,
-                                           blackListLayerNames );
+                        blackListLayerNames );
+    }
+
+    private AttributeModifier createAttributeModifier() {
+        List<AttributeModificationRule> getRules = createGetRules();
+        List<AttributeModificationRule> postRules = createPostRules();
+        if ( !getRules.isEmpty() || !postRules.isEmpty() ) {
+            List<AttributeModificationRule> rules = new LinkedList<AttributeModificationRule>();
+            rules.addAll( getRules );
+            rules.addAll( postRules );
+            return new StaticAttributeModifier( rules, "href", "http://www.w3.org/1999/xlink" );
+        }
+        return null;
     }
 
     private void checkVersion( OwsRequest owsRequest ) {
@@ -105,12 +148,43 @@ public class WmsCapabilitiesModificationManagerCreator implements XmlModificatio
 
     private boolean isWms130GetMapPermission( RasterPermission permission ) {
         return WMS_SERVICE.equalsIgnoreCase( permission.getServiceType() )
-               && permission.getServiceVersion().contains( VERSION_130 ) && isGetMapOrFeaturinfo( permission );
+               && permission.getServiceVersion().contains( VERSION_130 ) && isGetMapOrFeatureInfo( permission );
     }
 
-    private boolean isGetMapOrFeaturinfo( RasterPermission permission ) {
+    private boolean isGetMapOrFeatureInfo( RasterPermission permission ) {
         return GETMAP.equalsIgnoreCase( permission.getOperationType() )
                || GETFEATUREINFO.equalsIgnoreCase( permission.getOperationType() );
+    }
+
+    private List<AttributeModificationRule> createGetRules() {
+        return createRules( getDcpUrl, "Get" );
+    }
+
+    private List<AttributeModificationRule> createPostRules() {
+        return createRules( postDcpUrl, "Post" );
+    }
+
+    private List<AttributeModificationRule> createRules( String url, String getOrPost ) {
+        List<AttributeModificationRule> rules = new ArrayList<AttributeModificationRule>();
+        if ( url != null ) {
+            rules.add( new AttributeModificationRule( url, createPath( "GetCapabilities", getOrPost ) ) );
+            rules.add( new AttributeModificationRule( url, createPath( "GetMap", getOrPost ) ) );
+            rules.add( new AttributeModificationRule( url, createPath( "GetFeatureInfo", getOrPost ) ) );
+        }
+        return rules;
+    }
+
+    private LinkedList<ElementPathStep> createPath( String operationType, String getOrPost ) {
+        LinkedList<ElementPathStep> path = new LinkedList<ElementPathStep>();
+        path.add( new ElementPathStep( new QName( "http://www.opengis.net/wms", "WMS_Capabilities" ) ) );
+        path.add( new ElementPathStep( new QName( "http://www.opengis.net/wms", "Capability" ) ) );
+        path.add( new ElementPathStep( new QName( "http://www.opengis.net/wms", "Request" ) ) );
+        path.add( new ElementPathStep( new QName( "http://www.opengis.net/wms", operationType ) ) );
+        path.add( new ElementPathStep( new QName( "http://www.opengis.net/wms", "DCPType" ) ) );
+        path.add( new ElementPathStep( new QName( "http://www.opengis.net/wms", "HTTP" ) ) );
+        path.add( new ElementPathStep( new QName( "http://www.opengis.net/wms", getOrPost ) ) );
+        path.add( new ElementPathStep( new QName( "http://www.opengis.net/wms", "OnlineResource" ) ) );
+        return path;
     }
 
 }
